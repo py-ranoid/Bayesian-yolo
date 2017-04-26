9a1cb632b363b0b611d58c42b1c00e20bd72108b
xiaohang <hang_xiao_cn@163.com>
2017-04-26 13:43:23 +0800

    add load_darknet_weights, version 0.0

--- test_tiny_yolo.py
+++ test_tiny_yolo.py
@@ -5,61 +5,62 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 import math
+from collections import OrderedDict
 
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.cnn1 = nn.Sequential(
+        self.cnn1 = nn.Sequential(OrderedDict([
             # conv1
-            nn.Conv2d( 3, 16, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(16),
-            nn.LeakyReLU(0.1, inplace=True),
-            nn.MaxPool2d(2, 2),
+            ('conv1', nn.Conv2d( 3, 16, 3, 1, 1, bias=False)),
+            ('bn1', nn.BatchNorm2d(16)),
+            ('leaky1', nn.LeakyReLU(0.1, inplace=True)),
+            ('pool1', nn.MaxPool2d(2, 2)),
 
             # conv2
-            nn.Conv2d(16, 32, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(0.1, inplace=True),
-            nn.MaxPool2d(2, 2),
+            ('conv2', nn.Conv2d(16, 32, 3, 1, 1, bias=False)),
+            ('bn2', nn.BatchNorm2d(32)),
+            ('leaky2', nn.LeakyReLU(0.1, inplace=True)),
+            ('pool2', nn.MaxPool2d(2, 2)),
 
             # conv3
-            nn.Conv2d(32, 64, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.1, inplace=True),
-            nn.MaxPool2d(2, 2),
+            ('conv3', nn.Conv2d(32, 64, 3, 1, 1, bias=False)),
+            ('bn3', nn.BatchNorm2d(64)),
+            ('leaky3', nn.LeakyReLU(0.1, inplace=True)),
+            ('pool3', nn.MaxPool2d(2, 2)),
 
             # conv4
-            nn.Conv2d(64, 128, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.1, inplace=True),
-            nn.MaxPool2d(2, 2),
+            ('conv4', nn.Conv2d(64, 128, 3, 1, 1, bias=False)),
+            ('bn4', nn.BatchNorm2d(128)),
+            ('leaky4', nn.LeakyReLU(0.1, inplace=True)),
+            ('pool4', nn.MaxPool2d(2, 2)),
 
             # conv5
-            nn.Conv2d(128, 256, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.1, inplace=True),
-            nn.MaxPool2d(2, 2),
+            ('conv5', nn.Conv2d(128, 256, 3, 1, 1, bias=False)),
+            ('bn5', nn.BatchNorm2d(256)),
+            ('leaky5', nn.LeakyReLU(0.1, inplace=True)),
+            ('pool5', nn.MaxPool2d(2, 2)),
 
             # conv6
-            nn.Conv2d(256, 512, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.1, inplace=True),
-        )
+            ('conv6', nn.Conv2d(256, 512, 3, 1, 1, bias=False)),
+            ('bn6', nn.BatchNorm2d(512)),
+            ('leaky6', nn.LeakyReLU(0.1, inplace=True)),
+        ]))
 
-        self.cnn2 = nn.Sequential(
+        self.cnn2 = nn.Sequential(OrderedDict([
             # conv7
-            nn.Conv2d(512, 1024, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(1024),
-            nn.LeakyReLU(0.1, inplace=True),
+            ('conv7', nn.Conv2d(512, 1024, 3, 1, 1, bias=False)),
+            ('bn7', nn.BatchNorm2d(1024)),
+            ('leaky7', nn.LeakyReLU(0.1, inplace=True)),
 
             # conv8
-            nn.Conv2d(1024, 1024, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(1024),
-            nn.LeakyReLU(0.1, inplace=True),
+            ('conv8', nn.Conv2d(1024, 1024, 3, 1, 1, bias=False)),
+            ('bn8', nn.BatchNorm2d(1024)),
+            ('leaky8', nn.LeakyReLU(0.1, inplace=True)),
 
             # output
-            nn.Conv2d(1024, 125, 1, 1, 0),
-        )
+            ('output', nn.Conv2d(1024, 125, 1, 1, 0)),
+        ]))
 
     def forward(self, x):
         x = F.max_pool2d(F.pad(self.cnn1(x), (0,1,0,1), mode='replicate'), 2, stride=1)
@@ -67,30 +68,46 @@ class Net(nn.Module):
         return x
         #return F.log_softmax(x)
 
-def load_conv(buf, start, conv_model):
-    num_w = conv_model.weight.numel()
-    num_b = conv_model.bias.numel()
-    conv_model.bias.data.copy_(torch.from_numpy(buf[start:start+num_b])); start = start + num_b
-    conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w
-    return start
+    def load_darknet_weights(self, path):
+        def load_conv(buf, start, conv_model):
+            num_w = conv_model.weight.numel()
+            num_b = conv_model.bias.numel()
+            conv_model.bias.data.copy_(torch.from_numpy(buf[start:start+num_b])); start = start + num_b
+            conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w
+            return start
+        
+        def load_conv_bn(buf, start, conv_model, bn_model):
+            num_w = conv_model.weight.numel()
+            num_b = bn_model.bias.numel()
+            bn_model.bias.data.copy_(torch.from_numpy(buf[start:start+num_b])); start = start + num_b
+            bn_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_b])); start = start + num_b
+            bn_model.running_mean.copy_(torch.from_numpy(buf[start:start+num_b])); start = start + num_b
+            running_var = torch.from_numpy(buf[start:start+num_b]); start = start + num_b
+            bn_model.running_var.copy_((running_var.sqrt() + .00001).pow(2) - 0.00001)
+            conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w 
+            return start
+        
+        def load_fc(buf, start, fc_model):
+            num_w = fc_model.weight.numel()
+            num_b = fc_model.bias.numel()
+            fc_model.bias.data.copy_(torch.from_numpy(buf[start:start+num_b])); start = start + num_b
+            fc_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w 
+            return start
 
-def load_conv_bn(buf, start, conv_model, bn_model):
-    num_w = conv_model.weight.numel()
-    num_b = bn_model.bias.numel()
-    bn_model.bias.data.copy_(torch.from_numpy(buf[start:start+num_b])); start = start + num_b
-    bn_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_b])); start = start + num_b
-    bn_model.running_mean.copy_(torch.from_numpy(buf[start:start+num_b])); start = start + num_b
-    running_var = torch.from_numpy(buf[start:start+num_b]); start = start + num_b
-    bn_model.running_var.copy_((running_var.sqrt() + .00001).pow(2) - 0.00001)
-    conv_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w 
-    return start
-
-def load_fc(buf, start, fc_model):
-    num_w = fc_model.weight.numel()
-    num_b = fc_model.bias.numel()
-    fc_model.bias.data.copy_(torch.from_numpy(buf[start:start+num_b])); start = start + num_b
-    fc_model.weight.data.copy_(torch.from_numpy(buf[start:start+num_w])); start = start + num_w 
-    return start
+        #buf = np.fromfile('tiny-yolo-voc.weights', dtype = np.float32)
+        buf = np.fromfile(path, dtype = np.float32)
+        start = 4
+        
+        start = load_conv_bn(buf, start, self.cnn1[0], self.cnn1[1])
+        start = load_conv_bn(buf, start, self.cnn1[4], self.cnn1[5])
+        start = load_conv_bn(buf, start, self.cnn1[8], self.cnn1[9])
+        start = load_conv_bn(buf, start, self.cnn1[12], self.cnn1[13])
+        start = load_conv_bn(buf, start, self.cnn1[16], self.cnn1[17])
+        start = load_conv_bn(buf, start, self.cnn1[20], self.cnn1[21])
+        
+        start = load_conv_bn(buf, start, self.cnn2[0], self.cnn2[1])
+        start = load_conv_bn(buf, start, self.cnn2[3], self.cnn2[4])
+        start = load_conv(buf, start, self.cnn2[6])
 
 def sigmoid(x):
     return 1.0/(math.exp(-x)+1.)
@@ -153,23 +170,11 @@ def plot_boxes(img, boxes):
         draw.rectangle([x1, y1, x2, y2])
     img.save('predict.png')
 
+############################################
 m = Net() 
 m.float()
 m.eval()
-
-buf = np.fromfile('tiny-yolo-voc.weights', dtype = np.float32)
-start = 4
-
-start = load_conv_bn(buf, start, m.cnn1[0], m.cnn1[1])
-start = load_conv_bn(buf, start, m.cnn1[4], m.cnn1[5])
-start = load_conv_bn(buf, start, m.cnn1[8], m.cnn1[9])
-start = load_conv_bn(buf, start, m.cnn1[12], m.cnn1[13])
-start = load_conv_bn(buf, start, m.cnn1[16], m.cnn1[17])
-start = load_conv_bn(buf, start, m.cnn1[20], m.cnn1[21])
-
-start = load_conv_bn(buf, start, m.cnn2[0], m.cnn2[1])
-start = load_conv_bn(buf, start, m.cnn2[3], m.cnn2[4])
-start = load_conv(buf, start, m.cnn2[6])
+m.load_darknet_weights('tiny-yolo-voc.weights')
 
 img = Image.open('person.jpg').convert('RGB')
 boxes = do_detect(m, img)
