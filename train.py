6c7c1561b42804f4d50d34e0df220c913711f064
xiaohang <hang_xiao_cn@163.com>
2018-01-10 17:15:49 +0800

    find a bug in eval.py, add best_j

--- train.py
+++ train.py
@@ -239,10 +239,13 @@ def test(epoch):
             for i in range(num_gts):
                 box_gt = [truths[i][1], truths[i][2], truths[i][3], truths[i][4], 1.0, 1.0, truths[i][0]]
                 best_iou = 0
+                best_j = -1
                 for j in range(len(boxes)):
                     iou = bbox_iou(box_gt, boxes[j], x1y1x2y2=False)
-                    best_iou = max(iou, best_iou)
-                if best_iou > iou_thresh and boxes[j][6] == box_gt[6]:
+                    if iou > best_iou:
+                        best_j = j
+                        best_iou = iou
+                if best_iou > iou_thresh and boxes[best_j][6] == box_gt[6]:
                     correct = correct+1
 
     precision = 1.0*correct/(proposals+eps)
