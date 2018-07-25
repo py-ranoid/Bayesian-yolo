b21e2a4ca11dd4405abdb2cf2c0b34f64b9c7b35
Vishal Gupta <vishalg8897@gmail.com>
2018-07-25 21:06:33 +0530

    Added functions to load bayesian modules with and without batchnorm

--- cfg.py
+++ cfg.py
@@ -170,12 +170,21 @@ def print_cfg(blocks):
 
 
 def load_conv(buf, start, conv_model):
-    num_w = conv_model.weight.numel()
-    num_b = conv_model.bias.numel()
+    num_w = conv_model.weight.data.numel()
+    num_b = conv_model.bias.data.numel()
     conv_model.bias.data.copy_(torch.from_numpy(buf[start:start + num_b]))
     start = start + num_b
     conv_model.weight.data.copy_(torch.from_numpy(
         buf[start:start + num_w]).view(conv_model.weight.data.shape))
+    start = start + num_w
+    return start
+
+def load_conv_bayes(buf, start, conv_model):
+    num_w = conv_model.weight_mu.numel()
+    num_b = conv_model.bias_mu.numel()
+    conv_model.bias_mu.data.copy_(torch.from_numpy(buf[start:start + num_b]))
+    start = start + num_b
+    conv_model.weight_mu.data.copy_(torch.from_numpy(buf[start:start + num_w]).view(conv_model.weight_mu.data.shape))
     start = start + num_w
     return start
 
@@ -200,11 +209,30 @@ def load_conv_bn(buf, start, conv_model, bn_model):
     start = start + num_b
     bn_model.running_var.copy_(torch.from_numpy(buf[start:start + num_b]))
     start = start + num_b
-    conv_model.weight.data.copy_(torch.from_numpy(
-        buf[start:start + num_w]).view(conv_model.weight.data.shape))
+    conv_model.weight.data.copy_(torch.reshape(torch.from_numpy(buf[start:start+num_w]),
+                                               (conv_model.weight.shape[0],conv_model.weight.shape[1], conv_model.weight.shape[2],conv_model.weight.shape[3])
+                                               )
+                                 );
     start = start + num_w
     return start
 
+def load_conv_bn_bayes(buf, start, conv_model, bn_model):
+    num_w = conv_model.weight_mu.numel()
+    num_b = bn_model.bias.numel()
+    bn_model.bias.data.copy_(torch.from_numpy(buf[start:start + num_b]))
+    start = start + num_b
+    bn_model.weight.data.copy_(torch.from_numpy(buf[start:start + num_b]))
+    start = start + num_b
+    bn_model.running_mean.copy_(torch.from_numpy(buf[start:start + num_b]))
+    start = start + num_b
+    bn_model.running_var.copy_(torch.from_numpy(buf[start:start + num_b]))
+    start = start + num_b
+    conv_model.weight_mu.data.copy_(torch.reshape(torch.from_numpy(buf[start:start+num_w]),
+                                                  (conv_model.weight_mu.shape[0],conv_model.weight_mu.shape[1], conv_model.weight_mu.shape[2],conv_model.weight_mu.shape[3])
+                                                  )
+                                    );
+    start = start + num_w
+    return start
 
 def save_conv_bn(fp, conv_model, bn_model):
     if bn_model.bias.is_cuda:
